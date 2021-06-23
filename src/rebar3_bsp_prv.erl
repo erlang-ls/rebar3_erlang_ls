73f83aa4b16499418f72f6ab6a979cdc17da74db
Louai Al-Khanji <louai@fb.com>
2021-06-23 17:20:14 +0100

    Stop depending on the compile pass unconditionally

--- src/rebar3_bsp_prv.erl
+++ src/rebar3_bsp_prv.erl
@@ -3,7 +3,7 @@
 -export([init/1, do/1, format_error/1]).
 
 -define(PROVIDER, bsp).
--define(DEPS, [compile]).
+-define(DEPS, []).
 -define(AGENT, rebar3_bsp_server).
 -define(NAME_PREFIX, "rebar3_bsp_").
 

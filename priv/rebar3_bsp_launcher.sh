fe4d6a5c1f814b92f872060c75c83cb73e6b5f4e
Louai Al-Khanji <louai@fb.com>
2021-06-24 12:17:10 +0100

    add some comments

--- priv/rebar3_bsp_launcher.sh
+++ priv/rebar3_bsp_launcher.sh
@@ -1,4 +1,17 @@
 #!/bin/sh -eu
+
+# Rebind stdio fds to new ones
+exec 3<&0 # stdin dup'd to fd 3
+exec 4<&1 # stdout dup'd to fd 4
+exec 5<&2 # stderr dup'd to fd 5
+
+# Redirect stdio fds to safety
+exec 0</dev/null # no input on fd 0
+exec 1>>/tmp/rebar3_bsp.log # log output
+exec 2>&1 # redirect stderr output to stdout
+
+# Inform the BSP plugin of our shenanigans
 export REBAR3_BSP_IO_FDS="3 4 5"
-exec "$@" 3<&0 4<&1 5<&2 0</dev/null 1>>/tmp/rebar3_bsp.log 2>&1
+# Invoke the command we were given
+exec "$@"
 

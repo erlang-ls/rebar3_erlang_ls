a975790f08c4fbb89b4bd977b1564d538e6a3f71
Louai Al-Khanji <louai@fb.com>
2021-06-25 19:57:46 +0100

    Rewrite echo port internals to use a state map

--- src/rebar3_bsp_echo_port.erl
+++ src/rebar3_bsp_echo_port.erl
@@ -22,6 +22,10 @@
 %%==============================================================================
 -type endpoints() :: {pid(), pid()}.
 -type debug_opts() :: [sys:dbg_opt()].
+-type state() :: #{ parent := pid()
+                  , endpoints := undefined | endpoints()
+                  , deb := debug_opts()
+                  }.
 
 %%==============================================================================
 %% API
@@ -51,70 +55,62 @@ set_endpoints(Pid, {A, B}) when is_pid(A), is_pid(B) ->
 init(Parent) ->
   ok = proc_lib:init_ack({ok, self()}),
   Deb = sys:debug_options([]),
-  loop(undefined, Parent, Deb).
+  loop(#{ parent => Parent, endpoints => undefined, deb => Deb }).
 
--spec system_terminate(any(), pid(), debug_opts(), endpoints() | undefined) -> no_return().
-system_terminate(Reason, _Parent, _Deb, Endpoints) ->
-  terminate(Endpoints, Reason).
+-spec system_terminate(any(), pid(), debug_opts(), state()) -> no_return().
+system_terminate(Reason, Parent, Deb, State) ->
+  #{ parent := Parent, deb := Deb } = State, %% sanity check
+  terminate(Reason, State).
 
--spec system_continue(pid(), debug_opts(), endpoints() | undefined) -> no_return().
-system_continue(Parent, Deb, Endpoints) ->
-  loop(Endpoints, Parent, Deb).
+-spec system_continue(pid(), debug_opts(), state()) -> no_return().
+system_continue(Parent, Deb, State) ->
+  #{ parent := Parent, deb := Deb } = State, %% sanity check
+  loop(State).
 
 %%==============================================================================
 %% Internal Functions
 %%==============================================================================
 
--spec loop(endpoints() | undefined, pid(), debug_opts()) -> no_return().
-loop(undefined, Parent, Deb) ->
+-spec loop(state()) -> no_return().
+loop(State) ->
   receive
-    {From, Ref, {set_endpoints, {A, B}}} ->
-      handle_set_endpoints(From, Ref, {A, B}, Parent, Deb);
+    {From, Ref, {set_endpoints, {A, B}}} when map_get(endpoints, State) =:= undefined ->
+      handle_set_endpoints(From, Ref, {A, B}, State);
+    {From, {command, Data}} when map_get(endpoints, State) =/= undefined ->
+      handle_command(From, Data, State);
     {From, close} ->
-      handle_close(From, undefined, Parent, Deb);
+      handle_close(From, State);
     {system, From, Request} ->
-      handle_system_msg(From, Request, undefined, Parent, Deb)
-  end;
-loop({A, B}, Parent, Deb) when is_pid(A) andalso is_pid(B) ->
-  receive
-    {From, {command, Data}}  ->
-      handle_command(From, Data, {A, B}, Parent, Deb);
-    {From, close} ->
-      handle_close(From, {A, B}, Parent, Deb);
-    {system, From, Request} ->
-      handle_system_msg(From, Request, {A, B}, Parent, Deb)
+      handle_system_msg(From, Request, State)
   end.
 
--spec handle_set_endpoints(pid(), any(), endpoints(), pid(), debug_opts()) -> no_return().
-handle_set_endpoints(From, Ref, {A, B}, Parent, Deb) ->
-  case is_pid(A) andalso is_pid(B) of
-    true ->
-      From ! {Ref, ok},
-      loop({A, B}, Parent, Deb);
-    false ->
-      error(badarg)
-  end.
+-spec handle_set_endpoints(pid(), any(), {pid(), pid()}, state()) -> no_return().
+handle_set_endpoints(From, Ref, {A, B}, #{ endpoints := undefined } = State) when is_pid(A) andalso is_pid(B) ->
+  From ! {Ref, ok},
+  loop(State#{ endpoints => {A, B} }).
 
--spec handle_command(pid(), any(), endpoints(), pid(), debug_opts()) -> no_return().
-handle_command(From, Data, {A, B}, Parent, Deb) ->
-  {_Sender, Receiver} = get_sender_receiver({A, B}, From),
+-spec handle_command(pid(), any(), state()) -> no_return().
+handle_command(From, Data, State) ->
+  {_Sender, Receiver} = get_sender_receiver(From, State),
   Receiver ! {self(), {data, Data}},
-  loop({A, B}, Parent, Deb).
+  loop(State).
 
--spec handle_close(pid(), endpoints() | undefined, pid(), debug_opts()) -> no_return().
-handle_close(From, {A, B}, _Parent, _Deb) ->
-  {Sender, _Receiver} = get_sender_receiver({A, B}, From),
+-spec handle_close(pid(), state()) -> no_return().
+handle_close(_From, #{ endpoints := undefined } = _State) ->
+  exit(closed);
+handle_close(From, State) ->
+  {Sender, _Receiver} = get_sender_receiver(From, State),
   Sender ! {self(), closed},
-  terminate({A, B}, closed);
-handle_close(_From, undefined, _Parent, _Deb) ->
-  exit(closed).
+  terminate(closed, State).
 
--spec handle_system_msg({pid(), any()}, any(), endpoints() | undefined, pid(), debug_opts()) -> no_return().
-handle_system_msg(From, Request, Endpoints, Parent, Deb) ->
-  sys:handle_system_msg(Request, From, Parent, ?MODULE, Deb, Endpoints).
+-spec handle_system_msg(any(), any(), state()) -> no_return().
+handle_system_msg(From, Request, State) ->
+  #{ parent := Parent, deb := Deb } = State,
+  sys:handle_system_msg(Request, From, Parent, ?MODULE, Deb, State).
 
--spec terminate(endpoints() | undefined, any()) -> no_return().
-terminate(Endpoints, Reason) ->
+-spec terminate(any(), state()) -> no_return().
+terminate(Reason, State) ->
+  #{ endpoints := Endpoints } = State,
   case Endpoints of
     {A, B} ->
       send_exit(A, Reason),
@@ -129,8 +125,9 @@ send_exit(To, Reason) when is_pid(To) ->
   To ! {'EXIT', self(), Reason},
   ok.
 
--spec get_sender_receiver(endpoints(), pid()) -> {pid(), pid()}.
-get_sender_receiver({A, B}, From) ->
+-spec get_sender_receiver(pid(), state()) -> {pid(), pid()}.
+get_sender_receiver(From, State) ->
+  #{ endpoints := {A, B} } = State,
   case From of
     A ->
       {A, B};
